47698fde9edd91baa2b8ffc1e56eb6cb80b24e96
santosh_pakhrin <santoshpakhrin61@gmail.com>
2024-10-10 17:36:23 +0545

    Add files via upload

---
+++ Hotel_Menu/main.js
@@ -0,0 +1,3 @@
+document.getElementById('header').innerHTML = "New Header Content";
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+document.getElementsByTagName("h4")[0].style.fontSize = "3vw";
